f17542877b2f6fbfd351087debae1c61061dd1b0
Naohisa Minagawa <info@clay-system.jp>
2014-06-26 20:38:07 +0900

    基本ファイルをコミット

---
+++ _configure/configure_iqp.in.php
@@ -0,0 +1,36 @@
+<?php
+// サイトコード
+Vizualizer_Configure::set("timezone", "Asia/Tokyo");
+
+// サイトコード
+Vizualizer_Configure::set("site_code", "shortUrl");
+
+// サイト名
+Vizualizer_Configure::set("site_name", "アフィリエイトシステム");
+
+// サイトメールアドレス
+Vizualizer_Configure::set("site_email", "minagawa@dot-hacks.com");
+
+// アクセスドメイン
+Vizualizer_Configure::set("site_domain", $_SERVER["SERVER_NAME"]);
+
+// サイトホームディレクトリ
+Vizualizer_Configure::set("site_home", realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . "..") . DIRECTORY_SEPARATOR . "templates");
+
+// データベースの接続設定
+Vizualizer_Configure::set("database", array(
+    "default" => array(
+        "read" => array(
+            "dbtype" => "mysql", "host" => "192.168.252.156",
+            "user" => "vizualizer", "password" => "VZP@ssw0rd",
+            "database" => "viz_shortage", "query" => "SET NAMES utf8"
+        ),
+        "write" => array(
+            "dbtype" => "mysql", "host" => "192.168.252.156",
+            "user" => "vizualizer", "password" => "VZP@ssw0rd",
+            "database" => "viz_shortage", "query" => "SET NAMES utf8"
+        )
+    )
+));
+
+Vizualizer_Configure::set("prefilters", array("VizualizerShortage"));
